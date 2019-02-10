1029d3b38606560e284109d4c29b27fdc037a6d5
Brett Winters <brettwinters@gmail.com>
2019-02-11 00:11:36 +0700

    Added MSTest

---
+++ d60.Cirqus.MSTest/d60.Cirqus.MSTest/CirqusTests.cs
@@ -0,0 +1,40 @@
+﻿using d60.Cirqus.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+
+namespace d60.Cirqus.MSTest
+{
+    public class CirqusTests : CirqusTestsHarness
+    {
+        public TestContext TestContext { get; protected set; }
+
+        [TestInitialize]
+        public void SetupInternal()
+        {
+            Begin(new ConsoleWriter());
+            Setup();
+        }
+
+        protected virtual void Setup()
+        {
+        }
+
+        [TestCleanup]
+        public void TeardownInternal()
+        {
+            Teardown();
+            End(TestContext.CurrentTestOutcome != UnitTestOutcome.Error);
+        }
+
+        protected virtual void Teardown()
+        {
+        }
+
+        [DebuggerStepThrough]
+        protected override void Fail()
+        {
+            Assert.Fail();
+        }
+    }
+}
